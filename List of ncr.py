fb67500158ea94644533d148a9aa676077491937
psycane <himanshumalhotra07@gmail.com>
2016-09-30 16:21:54 +0530

    Signed-off-by: psycane <himanshumalhotra07@gmail.com>

--- List of ncr.py
+++ List of ncr.py
@@ -1,8 +1,4 @@
-li=[]
-for i in xrange(1,(10**1)+3):
-    li.append([0]*(i))
-li[0][0]=1
-def ncr(n,r):
+def ncr(n,r,li):
     if n==r:
         li[n][r]=1
         return li[n][r]
@@ -14,12 +10,14 @@ def ncr(n,r):
         return li[n][r]
     if li[n][r]:
         return li[n][r]
-    li[n][r]=ncr(n-1,r)+ncr(n-1,r-1)
+    li[n][r]=ncr(n-1,r,li)+ncr(n-1,r-1,li)
     return li[n][r]
 
-ncr(10,10)
-print li
+li=[]
+for i in xrange(1,(10**1)+3):
+    li.append([0]*(i))
+li[0][0]=1
 for i in xrange(1,(10**1)):
     for j in xrange(0,i+1):
-        li[i][j]=ncr(i+1,j+1)
+        li[i][j]=ncr(i+1,j+1,li)
 print li
--- StartFile.py
+++
@@ -1,6 +0,0 @@
-import sys
-from fractions import gcd
-from math import *
-#list(map(int,sys.stdin.readline().split()))
-#map(int,sys.stdin.readline().split())
-#int(sys.stdin.readline())
