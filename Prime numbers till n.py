e1c91ca66674137d58a3226f83099d6957b6c2eb
psycane <himanshumalhotra07@gmail.com>
2017-01-17 21:56:13 +0530

    prime numbers till n optimised

--- Prime numbers till n.py
+++ Prime numbers till n.py
@@ -1,11 +1,15 @@
-import math
-def primes_sieve_best(n):
-    sieve=[True]*n
-    for i in xrange(3,int(n**0.5)+1,2):
+def primes(n):
+    correction = (n%6>1)
+    n = {0:n,1:n-1,2:n+4,3:n+3,4:n+2,5:n+1}[n%6]
+    sieve = [True] * (n//3)
+    sieve[0] = False
+    for i in range(int(n**0.5)//3+1):
         if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
-    return [2]+[i for i in xrange(3,n,2) if sieve[i]]
+            k=3*i+1|1
+            sieve[((k*k)//3)::2*k]=[False]*((n//6-(k*k)//6-1)//k+1)
+            sieve[(k*k+4*k-2*k*(i&1))//3::2*k]=[False]*((n//6-(k*k+4*k-2*k*(i&1))//6-1)//k+1)
+    return [2,3] + [3*i+1|1 for i in range(1,n//3-correction) if sieve[i]]
 
 
-li=primes_sieve_best((10**3)+1)
+li=primes((10**3)+1)
 print li
