dc71b76d235436a7cb35948baf13c7fcb8da02c5
psycane <himanshumalhotra07@gmail.com>
2017-01-17 21:52:30 +0530

    Connected components optimised

--- Connected Components using DFS.py
+++ Connected Components using DFS.py
@@ -1,19 +1,21 @@
-def connected_components(graph,n):
-    visited=[False]*n
+def connected_components(graph):
+    seen = set()
+    def dfs(v):
+        vs = set([v])
+        component=[]
+        while vs:
+            v = vs.pop()
+            seen.add(v)
+            vs |= set(graph[v]) - seen
+            component.append(v)
+        return component
     ans=[]
-    for i in graph:
-        if visited[i-1]==False:
-            ans.append(dfs(graph,i,visited,[]))
-    return ans
-
-def dfs(graph,v,visited,ans):
-    visited[v-1]=True
-    ans.append(v)
-    for i in graph[v]:
-        if visited[i-1]==False:
-            dfs(graph,i,visited,ans)
+    for v in graph:
+        if v not in seen:
+            d=dfs(v)
+            ans.append(d)
     return ans
 
 graph={1: [2, 3], 2: [1, 3], 3: [2, 1], 4: [5], 5: [4, 6], 6: [5, 7], 7: [6]}
 n=7
-print connected_components(graph,n)
+print connected_components(graph)
---
+++ isprime.py
@@ -0,0 +1,20 @@
+def isprime(n):
+    if n == 2 or n == 3:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
+    if n < 9:
+        return True
+    if n % 3 == 0:
+        return False
+    r = int(n**0.5)
+    f = 5
+    while f <= r:
+        if n % f == 0:
+            return False
+        if n % (f + 2) == 0:
+            return False
+        f += 6
+    return True
+
+print isprime(999000011)
